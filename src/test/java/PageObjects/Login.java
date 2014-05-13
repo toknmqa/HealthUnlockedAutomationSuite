a8e377a1733a4137e48c1b85c0a50c8f11bb9dd4
Panawat Iteeyaporn <piteey01@gmail.com>
2014-05-13 17:32:50 +0100

    Auto-Formatted the code structure.

--- src/test/java/PageObjects/Login.java
+++ src/test/java/PageObjects/Login.java
@@ -19,85 +19,92 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
 public class Login {
 
-    WebDriver driver;
-    public static String loginURL;
+	WebDriver driver;
+	public static String loginURL;
 
+	public Login() {
+		driver = SeleniumFactory.get();
+	}
 
+	public void openHomepage() {
+		if (driver.getCurrentUrl().equals("https://bg-test.www.ppgdes.com/")) {
+			System.out.println("This is the Homepage");
+		} else {
+			WebPageHelpers.openWebPage("homepage");
+		}
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException iex) {
+			iex.toString();
+		}
+	}
 
-    public Login() {
-        driver = SeleniumFactory.get();
-    }
+	// Login button home page
+	public void loginButton() {
+		WebElement webElement = driver.findElement(By
+				.cssSelector("a[data-reactid='.0.1.0.0.0']"));
+		Actions ac = new Actions(driver);
+		ac.click(webElement);
+	}
 
-    public void openHomepage() {
-        if(driver.getCurrentUrl().equals("https://bg-test.www.ppgdes.com/")) {
-            System.out.println("This is the Homepage");
-        }else{
-        WebPageHelpers.openWebPage("homepage");
-        }
-        try{
-        	Thread.sleep(10000);
-        	}
-        	catch(InterruptedException iex){
-        		iex.toString();
-        	}
-    }
-// Login button home page
-    public void loginButton(){
-    	WebElement webElement = driver.findElement(By.cssSelector("a[data-reactid='.0.1.0.0.0']"));
-    	Actions ac = new Actions(driver);
-    	ac.click(webElement);
-    	}    
- ///login page   
-    public void loginPage() {
-        if(driver.getCurrentUrl().equals("https://bg-test.www.ppgdes.com/login")) {
-        	loginURL ="True";
-        	System.out.println("Log in Page");
-        }else{
-        	loginURL ="False";
-        }
-        try{
-        	Thread.sleep(2000);
-        	}
-        	catch(InterruptedException iex){
-        		iex.toString();
-        	}
-    }
-    /*
-    public Boolean isNewsDropDownItemPresent(String newsItem){
-    	WebElement webElement = driver.findElement(By.linkText(newsItem));
-    	
-    	return webElement != null;
-    }*/
+	// /login page
+	public void loginPage() {
+		if (driver.getCurrentUrl().equals(
+				"https://bg-test.www.ppgdes.com/login")) {
+			loginURL = "True";
+			System.out.println("Log in Page");
+		} else {
+			loginURL = "False";
+		}
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException iex) {
+			iex.toString();
+		}
+	}
 
-    public Boolean verifyNewsDropDownItems(String newsItem) throws Throwable{
-    	WebElement webElement = WebPageHelpers.FindElementByCss("img[alt=\"News Section\"]");
-    	
-    	Actions ac = new Actions (driver);
-    	ac.moveToElement(webElement);
-    	ac.perform();
-    	
-    	//WebPageHelpers.waitForElement("dropdown-content daily_informer");
-    	
-    	WebDriverWait wait = new WebDriverWait(driver, 10);
-    	
-    	WebElement element = wait.until(
-    	        ExpectedConditions.visibilityOfElementLocated(By.className("daily_informer")));
+	/*
+	 * public Boolean isNewsDropDownItemPresent(String newsItem){ WebElement
+	 * webElement = driver.findElement(By.linkText(newsItem));
+	 * 
+	 * return webElement != null; }
+	 */
 
-    	
-    	try{
-    		WebElement dropDownElement = driver.findElement(By.linkText(newsItem));
-    		return dropDownElement.isDisplayed();
-    	}
-    	catch(NoSuchElementException ex){
-    		
-    		File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE); // Can use BASE64 or BYTES as OutputType
-    	    FileUtils.copyFile(scrFile, new File("C:\\Users\\Tim O'Keeffe\\Documents\\0001 Sunday Times top level navigation I should see a sub links " + newsItem + ".jpg"));
+	public Boolean verifyNewsDropDownItems(String newsItem) throws Throwable {
+		WebElement webElement = WebPageHelpers
+				.FindElementByCss("img[alt=\"News Section\"]");
 
-    		throw ex;
-    	}
-    	//Testing comment
-    }
+		Actions ac = new Actions(driver);
+		ac.moveToElement(webElement);
+		ac.perform();
+
+		// WebPageHelpers.waitForElement("dropdown-content daily_informer");
+
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+
+		WebElement element = wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.className("daily_informer")));
+
+		try {
+			WebElement dropDownElement = driver.findElement(By
+					.linkText(newsItem));
+			return dropDownElement.isDisplayed();
+		} catch (NoSuchElementException ex) {
+
+			File scrFile = ((TakesScreenshot) driver)
+					.getScreenshotAs(OutputType.FILE); // Can use BASE64 or
+														// BYTES as OutputType
+			FileUtils
+					.copyFile(
+							scrFile,
+							new File(
+									"C:\\Users\\Tim O'Keeffe\\Documents\\0001 Sunday Times top level navigation I should see a sub links "
+											+ newsItem + ".jpg"));
+
+			throw ex;
+		}
+		// Testing comment
+	}
 }
--- src/test/java/helpers/WebPageHelpers.java
+++ src/test/java/helpers/WebPageHelpers.java
@@ -7,47 +7,51 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class WebPageHelpers {
 
-    public static void openWebPage(String url) {
-        // load page through selenium 2.0
-        String fullURL = System.getProperty("baseUrl") + url;
-        if ((url.contains("homepage"))) {
-            fullURL =  "https://bg-test.www.ppgdes.com/";
-        }
+	public static void openWebPage(String url) {
+		// load page through selenium 2.0
+		String fullURL = System.getProperty("baseUrl") + url;
+		if ((url.contains("homepage"))) {
+			fullURL = "https://bg-test.www.ppgdes.com/";
+		}
 
-        System.out.println("Opening URL: " + fullURL);
-        SeleniumFactory.get(fullURL, true);
-        // Maximise browser window
-    }
-    
-    public static WebElement FindElementByCss(String CssLocator){
-        try{
-            return SeleniumFactory.get().findElement(By.cssSelector(CssLocator));
-        }catch (NoSuchElementException e){
-            return null;
-        }
-    }
+		System.out.println("Opening URL: " + fullURL);
+		SeleniumFactory.get(fullURL, true);
+		// Maximise browser window
+	}
 
-    public static List<WebElement> FindElementsByCss(String CssLocator){
-        try{
-            return SeleniumFactory.get().findElements(By.cssSelector(CssLocator));
-        }catch (NoSuchElementException e){
-            return null;
-        }
-    }
+	public static WebElement FindElementByCss(String CssLocator) {
+		try {
+			return SeleniumFactory.get()
+					.findElement(By.cssSelector(CssLocator));
+		} catch (NoSuchElementException e) {
+			return null;
+		}
+	}
 
+	public static List<WebElement> FindElementsByCss(String CssLocator) {
+		try {
+			return SeleniumFactory.get().findElements(
+					By.cssSelector(CssLocator));
+		} catch (NoSuchElementException e) {
+			return null;
+		}
+	}
 
-    public static boolean isElementPresentByCss(String CssLocator){
-        if(!FindElementByCss(CssLocator).equals(null)){
-            return true;
-        }else{
-            return false;
-        }
-    }
-    
-    public static void waitForElement(String CssLocator){
-        new WebDriverWait(SeleniumFactory.get(), 10, 50).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(CssLocator)));
-    }
+	public static boolean isElementPresentByCss(String CssLocator) {
+		if (!FindElementByCss(CssLocator).equals(null)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static void waitForElement(String CssLocator) {
+		new WebDriverWait(SeleniumFactory.get(), 10, 50)
+				.until(ExpectedConditions.visibilityOfElementLocated(By
+						.cssSelector(CssLocator)));
+	}
 
 }
