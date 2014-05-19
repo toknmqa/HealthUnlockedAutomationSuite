4a0d58cf736373f270f5debdce6c748a46d7fb0b
Panawat Iteeyaporn <piteey01@gmail.com>
2014-05-19 11:48:52 +0100

    Unused imports have been removed from Login class

--- src/test/java/PageObjects/Login.java
+++ src/test/java/PageObjects/Login.java
@@ -1,29 +1,9 @@
 package PageObjects;
 
-import java.io.File;
 import static org.junit.Assert.assertTrue;
-
-import java.io.IOException;
-import java.util.Collection;
-import java.util.Date;
-import java.util.List;
-import java.util.Random;
-
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-
-import helpers.SeleniumFactory;
 import helpers.WebPageHelpers;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Action;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Login extends WebPageHelpers {
@@ -37,10 +17,11 @@ public class Login extends WebPageHelpers {
 		if (page.equals("Homepage")) {
 			assertTrue(getDriver().getCurrentUrl().equals(
 					"https://bg-test.www.ppgdes.com/login"));
-			}else {
-			WebElement modal = getDriver()
-					.findElement(By.className("modal-content"));
-			
-			assertTrue(modal.isDisplayed());		}
+		} else {
+			WebElement modal = getDriver().findElement(
+					By.className("modal-content"));
+
+			assertTrue(modal.isDisplayed());
+		}
 	}
 }
